12b0bee11d976a319550320e30aeebab5becadc6
GundeepSingh <gundeepsingh814@gmail.com>
2019-07-23 20:30:32 +0530

    Updated and removed crashes

---
+++ app/src/main/java/com/bitfield/ostello/Studentss.java
@@ -0,0 +1,14 @@
+package com.bitfield.ostello;
+
+import java.util.ArrayList;
+
+class Studentss {
+    String rollno;
+    String roomno;
+    String address;
+    String phone;
+    ArrayList<Leave>Leaves = new ArrayList<>();
+    ArrayList<Complaint>Complaints = new ArrayList<>();
+    String name;
+    String UID;
+}
